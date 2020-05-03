5d669e13f7f8c4c8b72f7995763c3bc93c538981
AmaruEscalante <sebastian.escalante@utec.edu.pe>
2020-05-02 22:01:07 -0400

    Ejercicios 2,3,4

---
+++ build/CMakeFiles/tarea3.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/src/clases/LIFO.cpp" "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/build/CMakeFiles/tarea3.dir/src/clases/LIFO.cpp.o"
+  "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/src/clases/LIFO_Simple.cpp" "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/build/CMakeFiles/tarea3.dir/src/clases/LIFO_Simple.cpp.o"
+  "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/src/clases/Order.cpp" "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/build/CMakeFiles/tarea3.dir/src/clases/Order.cpp.o"
+  "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/src/clases/Product.cpp" "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/build/CMakeFiles/tarea3.dir/src/clases/Product.cpp.o"
+  "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/src/clases/User.cpp" "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/build/CMakeFiles/tarea3.dir/src/clases/User.cpp.o"
+  "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/src/clases/Vehicle.cpp" "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/build/CMakeFiles/tarea3.dir/src/clases/Vehicle.cpp.o"
+  "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/src/ejercicios.cpp" "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/build/CMakeFiles/tarea3.dir/src/ejercicios.cpp.o"
+  "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/src/main.cpp" "/home/amaru/Documents/CLionProjects/POO2_HW/tarea3/build/CMakeFiles/tarea3.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tarea3.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarea3.dir/src/ejercicios.cpp.o"
+  "CMakeFiles/tarea3.dir/src/main.cpp.o"
+  "CMakeFiles/tarea3.dir/src/clases/LIFO.cpp.o"
+  "CMakeFiles/tarea3.dir/src/clases/LIFO_Simple.cpp.o"
+  "CMakeFiles/tarea3.dir/src/clases/Order.cpp.o"
+  "CMakeFiles/tarea3.dir/src/clases/Product.cpp.o"
+  "CMakeFiles/tarea3.dir/src/clases/User.cpp.o"
+  "CMakeFiles/tarea3.dir/src/clases/Vehicle.cpp.o"
+  "tarea3.pdb"
+  "tarea3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tarea3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
